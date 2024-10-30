d94e68b289f238b39a5d457ca280a3ae211fc050
= <av3157@columbia.edu>
2024-10-30 00:27:55 -0400

    added node extracting function

---
+++ code/components/extract_node_info.py
@@ -0,0 +1,35 @@
+# imports
+from rapidfuzz import process, fuzz
+
+# Extract node information from user query 
+nodes = ["BusinessGroup", "Column", "Contact", "Database", 
+        "DataElement", "Model", "ModelVersion", "Report", 
+        "ReportField", "ReportSection", "Table", "User"]
+
+# generate bigrams from the user question  
+def generate_bigrams(words):
+    n = len(words)
+    return [" ".join(words[i:i + 2]) for i in range(n - 1)] + words
+
+# match node from the list with generated bigrams
+def match_node(user_input):
+    words = user_input.split()
+    bigrams = generate_bigrams(words)
+    
+    # Find the best matching node among the generated bigrams
+    best_match, best_score = None, 0
+    for phrase in bigrams:
+        match, score, idx = process.extractOne(phrase, nodes, scorer=fuzz.ratio)
+        if score > best_score:  # Track the best match
+            best_match, best_score = match, score
+    
+    # Return the best match if it meets the threshold
+    if best_score >= 70: 
+        return best_match + "_embedding_graph"
+    else: 
+        return None
+
+# Test
+user_input = "What is the busness grop of this company?" # purposely misspelled 
+result = match_node(user_input)
+print(result)
